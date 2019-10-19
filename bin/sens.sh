06d74c03309c302e82215161c1d1e0a23087d998
saysky <847064370@qq.com>
2019-10-19 21:40:26 +0800

    update

---
+++ bin/sens.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+APP_NAME=sens-latest.jar
+
+usage() {
+    echo "用法: sh sens.sh [start(启动)|stop(停止)|restart(重启)|status(状态)]"
+    exit 1
+}
+
+is_exist(){
+  pid=`ps -ef|grep $APP_NAME|grep -v grep|awk '{print $2}' `
+  if [ -z "${pid}" ]; then
+   return 1
+  else
+    return 0
+  fi
+}
+
+start(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo "${APP_NAME} 正在运行。 pid=${pid} ."
+  else
+    nohup java -server -Xms1024m -Xmx1024m -Xmn340m -Xloggc:/www/wwwroot/sens-gc.log -jar $APP_NAME > /dev/null 2>&1 &
+    echo "${APP_NAME}启动成功，请查看日志确保运行正常。"
+    fi
+}
+
+stop(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    kill -9 $pid
+    echo "${pid} 进程已被杀死，程序停止运行"
+  else
+    echo "${APP_NAME} 没有运行。"
+  fi
+}
+
+status(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo "${APP_NAME} 正在运行。Pid is ${pid}"
+  else
+    echo "${APP_NAME} 没有运行。"
+  fi
+}
+
+restart(){
+  stop
+  start
+}
+
+case "$1" in
+  "start")
+    start
+    ;;
+  "stop")
+    stop
+    ;;
+  "status")
+    status
+    ;;
+  "restart")
+    restart
+    ;;
+  *)
+    usage
+    ;;
+esac
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # 指定SENS的根目录，请按实际修改
-SENS_DIR="/Users/liuyanzhao/code/SENS"
-#SENS_DIR="/www/wwwroot/SENS"
+SENS_DIR="/www/wwwroot/SENS
 # 拉取最新的源码
 # git pull
-echo "代码拉取完毕！"
+#echo "代码拉取完毕！"
 
 # 进入SENS根目录
 cd $SENS_DIR
@@ -14,8 +13,8 @@ cd $SENS_DIR
 sh $SENS_DIR/bin/sens.sh stop
 
 # 执行打包
-mvn clean package -Pprod -Dmaven.test.skip=true
-echo "代码拉取完毕！"
+mvn clean package -Dmaven.test.skip=true
+echo "代码打包完毕！"
 
 # 进入打包好的SENS目录
 cd $SENS_DIR/target
